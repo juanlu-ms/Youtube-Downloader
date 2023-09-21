4fe8904df1f99e49233d7ea68442049d2b4bb520
Juan Luis Martín Soto <jl.msoto@alumnos.upm.es>
2023-09-21 13:47:43 +0200

    quitar tabulación innecesaria

--- EliminadorMP4.py
+++ EliminadorMP4.py
@@ -6,5 +6,5 @@ with os.scandir(carpeta) as ficheros:
     for fichero in ficheros:
         path = pathlib.Path(fichero.path)
         if path.suffix == ".mp4":
-                os.remove(fichero.path)
+            os.remove(fichero.path)
 print("Se han eliminado los archivos :)")
