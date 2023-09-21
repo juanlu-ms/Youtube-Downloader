0e29b1b01981825785d11e2328fbc26f51622944
juanlu-ms <105309481+juanlu-ms@users.noreply.github.com>
2023-09-21 10:22:55 +0200

    Actualización para garantizar funcionamiento

--- YtDownloader.py
+++ YtDownloader.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+from Convertidor_MP3_MP4 import Convertir
 
 link = input("Introduzca el link del vídeo\n")
 yt = YouTube(link)
@@ -10,9 +11,10 @@ yd = yt.streams.get_highest_resolution()
 tipo = input("¿Es música (1) o vídeo (2)?\n")
 
 if tipo == "1":
-    yd.download('') #Poner en el paréntesis la carpeta deseada si es música
-    Convertir('') #Para copnvertir el archivo MP4 a MP3
+    yd.download('C:/Users/juanl/OneDrive/Música')
+    Convertir('C:/Users/juanl/OneDrive/Música')
+
 elif tipo == "2":
-    yd.download('') #Poner en el paréntesis la carpeta deseada si es un vídeo
+    yd.download('C:/Users/juanl/OneDrive/Videos')
 
 print("Se ha completado la descarga :)")
