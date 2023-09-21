4c226023dff35a2f308f5325885a7a464637dd05
Juan Luis Martín Soto <jl.msoto@alumnos.upm.es>
2023-09-21 10:26:44 +0200

    Borrado de main de la clase

--- Convertidor_MP3_MP4.py
+++ Convertidor_MP3_MP4.py
@@ -12,13 +12,3 @@ def Convertir (archivo):
                 clip.audio.write_audiofile(f'{path.stem}.mp3', bitrate="320k")
                 shutil.move(f'{path.stem}.mp3', 'C:/Users/juanl/OneDrive/Música')
     print("Se han convertido los archivos :)")
-
-# archivo = input("Intoduzca la ruta del archivo\n")
-# with os.scandir(archivo) as ficheros:
-#     for fichero in ficheros:
-#         path = pathlib.Path(fichero.path)
-#         if path.suffix == ".mp4":
-#             clip = mp.VideoFileClip(fichero.path)
-#             clip.audio.write_audiofile(f'{path.stem}.mp3', bitrate="320k")
-#             shutil.move(f'{path.stem}.mp3', 'C:/Users/Juanlu/OneDrive/Música')
-# print("Se han convertido los archivos :)")
